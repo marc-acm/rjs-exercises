cde8fedcb9a9536e87434c00eb0f38edcea28fe7
marc-acm <marc.macariola@yaho.com>
2017-09-10 11:40:07 +0800

    babel and webpack

---
+++  webpack.config.js
@@ -0,0 +1,28 @@
+var HTMLWebpackPlugin = require('html-webpack-plugin');
+var html-WebpackPluginConfig = new HTMLWebpackPluginConfig
+HTMLWebpackPlugin({
+/* configuration object here */
+	template: __dirname + '/app/index.html'
+	filename: 'index.html',
+	inject: 'body' 
+
+}); 
+
+
+module.exports = {
+	entry: __dirname + '/app/index.js'
+	module: {
+		loaders:[
+		{
+			test:/\.js$/,
+			exclude: /node_nodules/,
+			loader: 'babel-loader'
+		}
+	  ]
+	}
+	output: {
+		filename:'transformed.js',
+		path: __dirname + '/build'
+	}
+	plugins: [HTMLWebpackPluginConfig]
+};
